97067fb0a8d07285655bae3c37fd0665d242fbb9
nischhas <nischhal.shrestha@metropolia.fi>
2022-10-29 15:17:52 +0300

    added fake db for call

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+ // fake database: ****************
+  const users = [
+  {
+    user_id: 1,
+    name: 'Foo Bar',
+    email: 'foo@bar.fi',
+    password: 'foobar',
+  },
+  {
+    user_id: 2,
+    name: 'Bar Foo',
+    email: 'bar@foo.fi',
+    password: 'barfoo',
+  },
+]; 
+
+app.get('/users', (req, res) => {
+  const cat = {
+    name: 'Frank',
+    birthdate: '2010-12-25',
+    weight: 5,
+  };
+  res.json(users);
+});  
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
