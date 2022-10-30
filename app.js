279782d98c08c85765b6a7c1bae15603a127909a
Xiaoming Ma <73438688+myxmxm@users.noreply.github.com>
2022-10-31 00:11:09 +0200

    allowed download image with file name

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port = 3000
 app.use(cors());
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('uploads')); 
 
 app.use('/news', newsRoute);
 
