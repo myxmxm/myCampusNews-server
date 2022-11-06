5d0183d70118b4fc73cae1f14b09d2bf890cebec
nischhas <nischhal.shrestha@metropolia.fi>
2022-11-06 19:37:31 +0200

    added sighUp function to auth contoller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,58 @@
 'use strict';
+const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-const { httpError } = require("../utils/errors");
+const { getAllUsers, insertUser } = require('../models/userModel');
+const { httpError } = require('../utils/errors');
 
 const login = (req, res, next) => {
   // TODO: add passport authenticate
-  passport.authenticate('local', {session: false}, (err, user, info)=>{
+  passport.authenticate('local', { session: false }, (err, user, info) => {
     console.log('local params', err, user, info);
-    if(err || !user){
-        // next(err);
-        next(httpError('email / password incorrect', 400));
+    if (err || !user) {
+      // next(err);
+      next(httpError('email / password incorrect', 400));
+      return;
+    }
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        //   next(err);
+        next(httpError('login error', 400));
         return;
       }
-      req.login(user, {session: false}, (err)=>{
-        if (err) {
-            //   next(err);
-            next(httpError('login error', 400));
-            return;
-          }
-      });
+    });
 
-      // generate a signed son web token with the contents of user object and return it in the response
-      const token = jwt.sign(user, process.env.JWT_SECRET);
-      return res.json({user: user, token: token});
+    // generate a signed son web token with the contents of user object and return it in the response
+    const token = jwt.sign(user, process.env.JWT_SECRET);
+    return res.json({ user: user, token: token });
   })(req, res, next);
 };
 
+const signUp = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.error('user_post validation', errors.array());
+    const err = httpError('data not valid', 400);
+    next(err);
+    return;
+  }
+  const allUsers = await getAllUsers();
+  let emailExists = false;
+  allUsers.map((user) => {
+    if (user.email == req.body.email) {
+      emailExists = true;
+    }
+  });
+
+  if (emailExists) {
+    res.json({ message: 'This user email already exist', status: 409 });
+    return;
+  }
+  const uid = await insertUser(req.body);
+  res.json({ message: `user added with id: ${uid}`, status: 200 });
+};
 
 module.exports = {
   login,
+  signUp,
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,16 +1,18 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const {login} = require('../controllers/authController');
+const { login, signUp } = require('../controllers/authController');
 const { user_post } = require('../controllers/userController');
-const {body} = require('express-validator');
+const { body } = require('express-validator');
 
 router.post('/login', login);
 
-router.post('/register', 
-        body("fullName").isLength({ min: 3 }),
-        body('email').isEmail(),
-        body('password').matches('(?=.*[A-Z]).{8,}'),
-        user_post);
+router.post(
+  '/register',
+  body('fullName').isLength({ min: 3 }),
+  body('email').isEmail(),
+  body('password').matches('(?=.*[A-Z]).{8,}'),
+  signUp
+);
 
 module.exports = router;
